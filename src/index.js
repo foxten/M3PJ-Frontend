aeba6b6a804356603187a00dd093bd75a459a5b7
Yann233 <yannxyang@sina.com>
2020-08-17 21:31:19 -0500

    frontend

--- src/index.js
+++ src/index.js
@@ -12,10 +12,12 @@ function fetchGraphs(){
 
 
 function renderGraph(graph){
-      const card = `<div class="card" data-graph-id= "${graph.id}">
-      <h2>Input your answer</h2>
-      <img src="${graph.image_url}.png" class="graph-img" />
-
+      const card = `<div class="card" data-graph-id= "${graph.id}" style="background-color:white;">
+      <h2>${graph.id}. Input your answer. </h2>
+      <img src="${graph.image_url}.png" class="graph-img" width="250" height="200"/>
+      <p> Y = mx+b</p>
+      <input type="text" id="mKey" value="m value"> 
+      <input type="text" id="bKey" value="b value">
       </div>`
       const collection = document.getElementById('graph-collection')
       collection.innerHTML += card
