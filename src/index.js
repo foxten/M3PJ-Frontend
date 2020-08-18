497e11ef3a7b9725d85d5aaffedde6fba77cc92f
Yann233 <yannxyang@sina.com>
2020-08-18 16:04:40 -0500

    clickNext?

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function renderGraph(graph){
       <img src="${graph.image_url}.png" class="graph-img" width="250" height="200"/>
       <br>
       <p>Y =  <input type="text" class="checkM" id="inputM" placeholder="m" size="3" /> x+ <input type="text" class="checkB" id="inputB" placeholder="b" size="3" />
-      <button class="clickNext"> Next </button>
+      <button class="clickNext" style="float: right;"> Next </button>
       </div>`
        const collection = document.getElementById('graph-collection')
        collection.innerHTML += card
@@ -24,18 +24,32 @@ function renderGraph(graph){
 function clickNext(){
 const clickNext = document.getElementsByClassName('clickNext')
 clickNext.addEventListener('click', function(event){
-  // const getElementById 
 
-  //compare inputM and inputB with json keys
-  //if correct, go to next, if wrong, show "retake" for one time
+const mBox = document.getElementById("inputM")
+const bBox = document.getElementById("inputB")
 
-  //if inputM === ${graph.M} && inputB ===${graph.B}
+const totalPoint = 2 * (graphs.length - 1)
+  if (parseInt(mBox.innerText) === graph.m  && parseInt(bBox.innerText) === graph.b ) {
+    //go to next graph problem
+} else {
+    //allow to retake once for the specific problem
+    //how to set retake time =1?
+      if(retakeTime=1)
+      //{retake the question}
+      {}
+      else {
+      //go to next problem
+      }
+
+  }
 
   const reqObj = {
       method: 'PATCH',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
         //???????? nothing to save
+        //user.id:UID,
+
       })
   }
 
