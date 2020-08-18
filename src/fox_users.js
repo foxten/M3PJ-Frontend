597f04bd70fdf226739ed668cc10db4e596d8bce
foxten <tenaysiafox@gmail.com>
2020-08-18 17:00:32 -0500

    User event listeners added

--- src/fox_users.js
+++ src/fox_users.js
@@ -1,6 +1,5 @@
 const userURL="http://localhost:3000/users"
 
-
 function newUserListener(){
     document.querySelector('#username').addEventListener('submit', function(event){
         event.preventDefault();
@@ -16,9 +15,61 @@ function newUserListener(){
         }
     fetch(userURL,reqObj)
         .then(resp => resp.json())
-            .then(respData => console.log(respData))
-})
+            .then(respData => userDashboard(respData))
+    })
+}
 
+function userDashboard(user){
+    const dashboard = document.querySelector('#dashboard')
+    dashboard.dataset.id = user.id
+    const userInfo = `
+    <h2>Welcome ${user.username}</h2>
+    <ul id="scores">Scores:</ul>
+    <button type="button" data-set-id=${user.id} id="new">Start a New Game!</button>
+    <button type="button" data-set-id=${user.id} id="edit-username">Edit Username</button>
+    `
+    dashboard.innerHTML = userInfo
+        user.sessions.forEach(session => {
+            const newItem = `<li>${session.score}</li>`
+            document.querySelector('#scores').innerHTML += newItem
+        })
+    dashboard.addEventListener('click', function(event){
+        if (event.target.id === 'edit-username'){
+            // const newForm = document.createElement('form')
+            // const inputField = document.createElement('input')
+            // inputField.type = 'text'
+            // inputField.value = user.username
+            // dashboard.innerHTML = inputField
+            dashboard.innerHTML = `
+            <form id="username">
+            <input type='text' placeholder="Username" id="username-update" value=${user.username}></input>
+            <input type='button' id="update" value='Update Username'></input>
+            </form>`
+
+            editUserInfo(event)
+        }
+    })
+}
+
+function editUserInfo(event){
+    const update = document.querySelector('#update')
+    update.addEventListener('click', function(event){
+        event.preventDefault()
+        const newUsername = document.querySelector('#username-update').value
+        const userId = document.querySelector('#dashboard').dataset.id
+
+        const reqObj = {
+            method: 'PATCH',
+            headers: {
+                'Content-Type':'application/json'
+            }, 
+            body: JSON.stringify({newUsername})
+        }
+
+        fetch(`${userURL}/${userId}`, reqObj)
+            .then(resp => resp)
+            
+    })
 }
 
 newUserListener()
